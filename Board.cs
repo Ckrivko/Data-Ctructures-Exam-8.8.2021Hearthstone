8d580a7adfc091d5bbcf9930ca297b385f54485c
Ckrivko <76474570+Ckrivko@users.noreply.github.com>
2022-12-17 00:53:53 +0200

    Add files via upload

---
+++ Board.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Board : IBoard
+{
+    private Dictionary<string, Card> cards;
+
+    public Board()
+    {
+        this.cards = new Dictionary<string, Card>();
+    }
+
+    public bool Contains(string name)
+    {
+        return this.cards.ContainsKey(name);
+    }
+
+    public int Count()
+    {
+        return this.cards.Count;
+    }
+
+    public void Draw(Card card)
+    {
+
+        if (this.cards.ContainsKey(card.Name)) throw new ArgumentException();
+
+        this.cards.Add(card.Name, card);
+    }
+
+    public IEnumerable<Card> GetBestInRange(int start, int end)
+    {
+
+        return this.cards.Values.Where(x => x.Score >= start && x.Score <= end)
+            .OrderByDescending(x => x.Level);
+
+    }
+
+    public void Heal(int health)
+    {
+        this.cards.Values.OrderBy(x => x.Health).First().Health += health;
+
+    }
+
+    public IEnumerable<Card> ListCardsByPrefix(string prefix)
+    {
+        return this.cards.Values.Where(x => x.Name.StartsWith(prefix))
+            .OrderBy(x => (int)x.Name[x.Name.Length - 1])
+            .ThenBy(x => x.Level);
+    }
+
+    public void Play(string attackerCardName, string attackedCardName)
+    {
+
+        if (!this.cards.ContainsKey(attackerCardName)
+            || !this.cards.ContainsKey(attackedCardName)) throw new ArgumentException();
+
+        if (cards[attackerCardName].Level != cards[attackedCardName].Level) throw new ArgumentException();
+
+        var attacker = cards[attackerCardName];
+        var defender = cards[attackedCardName];
+
+        if (!defender.IsDead && !attacker.IsDead)
+        {
+            defender.Health -= attacker.Damage;
+
+            if (defender.IsDead)
+            {
+                attacker.Score += defender.Level;
+            }
+
+        }
+
+    }
+
+    public void Remove(string name)
+    {
+        if (!this.cards.ContainsKey(name)) throw new ArgumentException();
+
+        this.cards.Remove(name);
+    }
+
+    public void RemoveDeath()
+    {
+        this.cards = this.cards.Where(x => x.Value.IsDead == false).ToDictionary(t => t.Key, t => t.Value);
+    }
+
+    public IEnumerable<Card> SearchByLevel(int level)
+    {
+        return this.cards.Values.Where(x => x.Level == level).OrderByDescending(x => x.Score);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var card = new Card("Best card", 10, 20, 5);
+        var card2 = new Card("Cool chocolate", 10, 15, 5);
+        var card3 = new Card("Cosmic magic", 6, 8, 3);
+        var card4 = new Card("Combat card", 10, 8, 2);
+
+        var board = new Board();
+
+        //Act
+        board.Draw(card);
+        board.Draw(card2);
+        board.Draw(card3);
+        board.Draw(card4);
+
+        var expected = new List<Card>()
+            {
+                new Card("Cosmic magic", 6, 8, 3),
+                new Card("Combat card", 10, 8, 2),
+                new Card("Cool chocolate", 10, 15, 5)
+            };
+
+        var result = board.ListCardsByPrefix("Co");
+
+        foreach (var ca in result)
+        {
+            Console.WriteLine($"{ca.Name} {ca.Damage} {ca.Score} {ca.Level}");
+        }
+    }
+}
